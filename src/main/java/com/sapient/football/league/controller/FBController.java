e85e0c24f91df5c804c4e396e0d763be53662374
Hiring <Hiring@WKWIN7681928.global.publicisgroupe.net>
2019-07-27 10:47:12 +0530

    adding initial maven structure with spring app init and a rest controller

---
+++ src/main/java/com/sapient/football/league/controller/FBController.java
@@ -0,0 +1,16 @@
+package com.sapient.football.league.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FBController {
+
+	@RequestMapping(path = "/hello", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public String hello() {
+		return "OK";
+	}
+}
